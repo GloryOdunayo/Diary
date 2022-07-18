4603fdd6a9c2052fb9fd90b745f069c876ec503c
Glory <okewoleglory@gmail.com>
2022-07-18 04:18:12 +0100

    Fixing bug

--- todo.js
+++ todo.js
@@ -74,3 +74,14 @@ function add() {
 const logout = () => {
     localStorage.removeItem("email");
 }
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "140px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  function notinIn() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
